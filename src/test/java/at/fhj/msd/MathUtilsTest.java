09f1534225b062621540c60e3caa3619b3977322
maryam taeed <maryam.taeid@edu.fh-joanneum.at>
2025-09-29 23:07:27 +0200

    modify test methods

---
+++ src/test/java/at/fhj/msd/MathUtilsTest.java
@@ -0,0 +1,18 @@
+package at.fhj.msd;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class MathUtilsTest {
+    @Test
+    @DisplayName ("test is even ()")
+    void isEventest(){
+        assertTrue(MathUtils.isEven(8));
+    }
+     @Test
+    @DisplayName ("test if the number is not even")
+    void isNotEventest(){
+        assertFalse(MathUtils.isEven(11));
+    }
+
+}
